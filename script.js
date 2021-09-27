e5b3f0426b1fc4de3206349cf4605f209a376c68
Mads Hermansen <mads.hermansen@valknut.design>
2021-09-27 21:54:23 +0200

    changes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-var controller = new ScrollMagic.Controller();
+// Go to top button
 
-var page = new ScrollMagic.Scene({
-    triggerElement: '.info'
-})
-.setClassToggle('.info', 'show')
-.addTo('controller')
+function gototop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
