dcba6a68cc457425d513efe212b864d9300f60c8
Mads Hermansen <mads.hermansenmads.hermansen@valknut.design>
2021-11-29 15:50:43 +0100

    almost

--- script.js
+++ script.js
@@ -17,12 +17,26 @@ $('#active').change(function() {
 });
 
 var prevScrollpos = window.pageYOffset;
+document.getElementById("navbar").style.top = "0";
+document.getElementById("menuchange").style.top = "20";
+
 window.onscroll = function() {
+  var scrollTop = $(window).scrollTop();
+  var pixels = window.innerHeight;
+  if (prevScrollpos > pixels*1/7) {
+      document.getElementById("navbar").style.background = "#0E1B31";
+    } else {
+      document.getElementById("navbar").style.background = "transparent";
+    }
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.visibility = "visible";
     document.getElementById("navbar").style.top = "0";
+    document.getElementById("menuchange").style.top = "20px";
   } else {
-    document.getElementById("navbar").style.top = "-75px";
+    document.getElementById("navbar").style.top = "-80px";
+    document.getElementById("navbar").style.visibility = "hidden";
+    document.getElementById("menuchange").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
 }
